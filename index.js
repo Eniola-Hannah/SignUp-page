e102435fe94fd8b83795f1875925751a7ad76f35
Eniola Hannah <heniola395@gmail.com>
2022-08-31 08:57:31 +0100

    A signUp page for student

---
+++ index.js
@@ -0,0 +1,39 @@
+
+allStudents = []
+if (localStorage.studentDetails){
+    allStudents = JSON.parse(localStorage.getItem("studentDetails"))
+}
+
+function addStudent(){
+    var signUp = {
+        firstname:firstName.value,
+        lastname:lastName.value,
+        email:useremail.value,
+        password:userPassword.value,
+        date:new Date(),
+        matricNumber:"SQI"+ Math.round(Math.random()*10000)
+
+    }
+
+    if(signUp.firstname==""||signUp.lastname==""||signUp.email==""||signUp.password==""){
+        display.innerText = "There's stil an unfilled field"
+    }else{
+        allStudents.push(signUp)
+        console.log(allStudents)
+        localStorage.setItem("studentDetails", JSON.stringify(allStudents))
+        display.innerHTML = "Dear "+ signUp.firstname + "<br> sign up to the school's portal was successful <br> at " + signUp.date
+
+
+        firstName.value = ""
+        lastName.value = ""
+        useremail.value = ""
+        firstName.value = ""
+        userPassword.value = ""
+    }
+}
+function search(){
+    window.location.href = "search.html"
+}
+function searchStudent(){
+    display.innerHTML = "First name;  "+ allStudents[index.value].firstname + ", <br>Last name;  " +allStudents[index.value].lastname + ", <br>Email;  " + allStudents[index.value].email + ", <br>Matric No.;  " + allStudents[index.value].matricNumber + ", <br>Signed up at " + allStudents[index.value].date
+}

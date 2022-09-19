6ee6bd9d46807695c9240338f3057bdd29f7573f
Eniola Hannah <heniola395@gmail.com>
2022-09-19 11:45:35 +0100

    Created a sigIn Page & made sure the functions for sign in are working well

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 allStudents = []
 if (localStorage.studentDetails){
     allStudents = JSON.parse(localStorage.getItem("studentDetails"))
@@ -16,7 +15,7 @@ function addStudent(){
     }
 
     if(signUp.firstname==""||signUp.lastname==""||signUp.email==""||signUp.password==""){
-        display.innerText = "There's stil an unfilled field"
+        display.innerText = "Fill out all unfilled fields"
     }else{
         allStudents.push(signUp)
         console.log(allStudents)
@@ -29,11 +28,54 @@ function addStudent(){
         useremail.value = ""
         firstName.value = ""
         userPassword.value = ""
+        
     }
 }
+
+function signIn(){
+    var logInName = userName.value
+    var logInPassword = signinPassword.value
+    var logIn = false
+    var allStudents = JSON.parse(localStorage.getItem("studentDetails"))
+
+    for (let index = 0; index < allStudents.length; index++) {
+        if ((logInName == allStudents[index].firstname||logInName==allStudents[index].email) && logInPassword == allStudents[index].password){
+            logIn = true
+        }
+    }
+    if(logIn){
+        window.location.href = "studentTable.html"
+    }else{
+        display.innerText = "Incorrect Name or Password"
+
+    }
+
+    userName.value = ""
+    signinPassword.value = ""
+}
+
+
+
+
+for (var index = 0; index < allStudents.length; index++) {
+    myTable.innerHTML += `
+    <tr>
+        <td> ${index+1}</td>
+        <td>${allStudents[index].firstname}</td>
+        <td>${allStudents[index].lastname}</td>
+        <td>
+            <button class="btn btn-danger"> Delete</button>
+            <button class="btn btn-success"> Edit</button>
+        </td> 
+    </tr>`
+    // console.log(allStudents[index].firstname)
+}   
+
+
 function search(){
     window.location.href = "search.html"
 }
 function searchStudent(){
+    allStudents = JSON.parse(localStorage.getItem("studentDetails"))
     display.innerHTML = "First name;  "+ allStudents[index.value].firstname + ", <br>Last name;  " +allStudents[index.value].lastname + ", <br>Email;  " + allStudents[index.value].email + ", <br>Matric No.;  " + allStudents[index.value].matricNumber + ", <br>Signed up at " + allStudents[index.value].date
 }
